d7e945cd318c3da7aa9709925a8380f5f13fe485
Chewie <chewie1985@gmail.com>
2021-04-04 14:53:09 -0600

    adding value arrays

--- script.js
+++ script.js
@@ -1,4 +1,105 @@
 // Assignment code here
+var lowCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = [
+  " ",
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+
+var lowYes = window.confirm("Would you like to include lower case letters?");
+var upYes = window.confirm("Would you like to include capital letters?");
+var numYes = window.confirm("Would you like to include numbers?");
+var charYes = window.confirm("Would you like to include special characters?");
+
+console.log(lowYes);
+console.log(upYes);
+console.log(numYes);
+console.log(charYes);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
